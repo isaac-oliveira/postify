170debf253a21256c4f68f5e861771f885530c3a
isaac-oliveira <isaac.oliveira@popcode.com.br>
2026-09-15 08:04:48 -0300

    refactor(assets): move favicon to public/

--- src/app/assets/icons/index.ts
+++ src/app/assets/icons/index.ts
@@ -1,5 +1,5 @@
 import Logo from '../../../../docs/assets/logo.png?url'
 import Icon from '../../../../docs/assets/icon.png?url'
-import Favicon from '../../../../docs/assets/favicon.png?url'
+import Favicon from '../../../../public/favicon.png?url'
 
 export { Logo, Icon, Favicon }
